a578a4bba45d09d8f79682490755b3bd03541f32
Matt Seidel <mseidel819@gmail.com>
2023-05-03 11:50:15 -0500

    add provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/layout";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon_io/favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="/favicon_io/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon_io/site.webmanifest" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon_io/favicon-32x32.png" />
+          <link rel="apple-touch-icon" href="/favicon_io/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon_io/site.webmanifest" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
