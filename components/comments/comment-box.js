d52a2058d239b013e1ea139c813ddae2ab695898
Matt Seidel <mseidel819@gmail.com>
2023-05-05 14:21:46 -0500

    comment submit loader

--- components/comments/comment-box.js
+++ components/comments/comment-box.js
@@ -1,10 +1,10 @@
 import Comment from "./comment";
 import styles from "./comment-box.module.css";
 const CommentBox = ({ slug, comments, upvote, deleteCommentHandler }) => {
-  const filteredComments = comments.filter(
-    (comment) => comment.articleId === slug
-  );
-  filteredComments.sort((a, b) => b.score - a.score);
+  const filteredComments = comments
+    .filter((comment) => comment.articleId === slug)
+    .sort((a, b) => b.createdAt - a.createdAt);
+
   if (filteredComments.length) {
     return (
       <div className={styles.content}>
